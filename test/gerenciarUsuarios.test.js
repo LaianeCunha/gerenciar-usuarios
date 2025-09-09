e1003a4c5318c43fa5c6f3a5ae332c72596ff25f
Laiane Cunha <laianecunha20@gmail.com>
2025-09-09 15:44:04 -0300

    Adicionando funções e testes

---
+++ test/gerenciarUsuarios.test.js
@@ -0,0 +1,17 @@
+const {retornarUsuarios, adicionarNovoUsuario} = require('../src/gerenciarUsuarios');
+const { expect } = require('chai');
+
+describe ('Testar as funções de Gestão de Usuários', function (){
+
+    it('Validar que posso adicionar um novo nome de usuário na lista', function(){
+        // Adicionar um novo nome na lista de usuários
+        adicionarNovoUsuario('Maria');        
+
+        // Retornar a lista de usuários na caixa lista de usuários
+        const listaDeUsuarios = retornarUsuarios();
+
+        // Comparar se o novo nome está no fim da lista de usuários
+        expect(listaDeUsuarios.at(-1)).to.equal('Maria');
+    });
+
+});
